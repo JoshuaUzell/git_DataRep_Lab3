af2cdbca11380ccd246df54f0e0c4586ad2ca6e5
unknown <joshuauzell@gmail.com>
2022-10-07 09:46:21 +0000

    My lab 2

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="App">
+                    <h1>My Footer in another component</h1>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="App">
+                    <h1>My Header in another component</h1>
+                </div>
+            </div>
+        );
+    }
+}
