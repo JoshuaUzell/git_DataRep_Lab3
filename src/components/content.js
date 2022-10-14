c55715c759b52e147cf718ea2708f720bdc5815e
JoshuaUzell <80922592+JoshuaUzell@users.noreply.github.com>
2022-10-14 10:58:36 +0100

    Update content.js

--- src/components/content.js
+++ src/components/content.js
@@ -3,7 +3,7 @@ import React from "react";
 export class Content extends React.Component {
     render() {
         return (
-            <div>
+            <div> //Draws out Hello World onto the page as well as the current time
                 <div className="App">
                     <h1>Hello World</h1>
                     <h2>It is {new Date().toLocaleTimeString()}.</h2>
