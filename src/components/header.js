f86e9e88053331fbe8ef7d6222ea3117495cdb13
JoshuaUzell <80922592+JoshuaUzell@users.noreply.github.com>
2022-10-14 11:02:39 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ export class Header extends React.Component {
     render() {
         return (
             <div>
-                <div className="App">
+                <div className="App"> {//Displays message saying we are in the header component}
                     <h1>My Header in another component</h1>
                 </div>
             </div>
