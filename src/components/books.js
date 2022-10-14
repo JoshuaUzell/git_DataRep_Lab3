aa87e96902ca8b291267720ef6c38d41d8d952bc
JoshuaUzell <80922592+JoshuaUzell@users.noreply.github.com>
2022-10-14 10:57:54 +0100

    Update books.js

--- src/components/books.js
+++ src/components/books.js
@@ -3,6 +3,7 @@ import { BookItem } from "./bookItem";
 
 export class Books extends React.Component {
     render() {
+        {/*Creates a map for the books array and displays info from Book Item*/}
         return this.props.books.map(
             (book) => {
                 return <BookItem book={book} key={book.isbn}></BookItem>
