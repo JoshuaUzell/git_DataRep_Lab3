7f0bfdfb48d99f6728c4c60631cb8c6dce53a912
JoshuaUzell <80922592+JoshuaUzell@users.noreply.github.com>
2022-10-14 11:01:31 +0100

    Update create.js

--- src/components/create.js
+++ src/components/create.js
@@ -3,7 +3,7 @@ import React from "react";
 export class Create extends React.Component {
     render() {
         return (
-            <div>
+            <div> {//Displays a message saying that we are in the create component}
                 <div className="App">
                     <h3>Hello there from Create Component</h3>
                 </div>
